8704066d91edf35902965fcf82f9504d5da1494a
cesar <cesarbasiliogomez@gmail.com>
2025-11-25 23:41:36 -0600

    add: implmentacion de middleware token

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,20 +1,28 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import React from "react";
 import { AppSidebar } from "@/components/app-sidebar";
-export default function RootLayout({
+
+export default async function ProtectedLayout({
     children,
-}: Readonly<{
+}: {
     children: React.ReactNode;
-}>) {
+}) {
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get("accessToken")?.value;
+
+    if (!accessToken) {
+        redirect("/login");
+    }
+
     return (
         <SidebarProvider>
             <AppSidebar />
             <main className="w-full">
                 <SidebarTrigger />
-                <div className="h-4" >
-                    {children}
-                </div>
+                <div className="h-4">{children}</div>
             </main>
         </SidebarProvider>
-
     );
 }
